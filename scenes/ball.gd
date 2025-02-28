06a2ded5894da11d7e6e298ae10a02c9d47d8212
Timothy Cope <kirbycope@gmail.com>
2025-02-27 16:36:20 -0800

    repack

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -5,7 +5,15 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
+func _input(event: InputEvent) -> void:
+	# Handle screen tap
+	if event is InputEventScreenTouch and event.pressed:
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
